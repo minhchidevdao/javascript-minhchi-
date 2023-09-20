5f4abba96d0b9e902c1e45cf5fe2c3c0e26ab639
Minh Chi <minhchi.dev@gmail.com>
2023-09-20 16:26:57 +0700

    update file console.js

--- console.js
+++ console.js
@@ -1,6 +1,6 @@
-
-
-
-
-let x = "volvo" + 15
-console.log(x)
+let text = `ABCDEFHIJKLMNOPQRSTUVMcdms `;
+let length = text.length
+//console.log(length)
+let text1 = `Apple, Banana, Kiwi`;
+let part = text1.substr(7,6);
+console.log(part)
